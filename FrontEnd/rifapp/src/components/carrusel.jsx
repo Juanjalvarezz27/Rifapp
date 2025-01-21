951e981ca3fa5c332c807ff3208c0e95f5571f09
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2025-01-21 17:09:05 -0400

    Carrusel Home

--- FrontEnd/rifapp/src/components/carrusel.jsx
+++ FrontEnd/rifapp/src/components/carrusel.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function carrusel() {
+function Carrusel() {
+  const images = [
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Cambia cada 3 segundos
+
+    return () => clearInterval(interval); // Limpiar el intervalo al desmontar el componente
+  }, []);
+
   return (
-    <div>
-       <div className="bg-blue-500 h-64 flex items-center justify-center text-white rounded-lg">
-          SLIDER
-        </div> 
+    <div className="grid gap-4">
+      <div>
+        <img
+          className="h-auto max-w-full rounded-lg"
+          src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg"
+          alt="Imagen fija"
+        />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((src, index) => (
+          <div key={index} className={`transition-opacity duration-500 ${index === currentIndex ? 'opacity-100' : 'opacity-50'}`}>
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src={src}
+              alt={`Imagen ${index + 1}`}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default carrusel
+export default Carrusel;
--- FrontEnd/rifapp/src/components/grid.jsx
+++ FrontEnd/rifapp/src/components/grid.jsx
@@ -4,8 +4,8 @@ import Carrusel from './carrusel';
 
 const GridComponent = () => {
   return (
-    <div className="flex justify-center items-center mt-6 px-5 " >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-3xl ">
+    <div className="flex justify-center items-center mt-9 px-5 " >
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-7xl">
         
        <Carrusel />
        <Login />

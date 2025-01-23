c725f1b4c2b44af36a6ee24681ca9699aaeb6047
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2025-01-23 10:58:42 -0400

    Completando el AboutUs

--- FrontEnd/rifapp/src/components/footer.jsx
+++ FrontEnd/rifapp/src/components/footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
 
   return (
     <>
-      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 justify-between items-center bg-primary-200 text-white-500'>
+      <div className='text grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 justify-between items-center bg-primary-200 text-white-500'>
         
         <div className="flex items-center justify-center w-full md:w-auto md:mt-5 sm:mt-5 xs:mt-5 xxs:mt-5">
           <p className='text-6xl font-bold title text-white-700'>RIFAPP</p>
@@ -26,16 +26,15 @@ const Footer = () => {
           <div className="w-full md:w-full mt-4 md:mt-0 lg:p-4">
             <div id="accordion-collapse" data-accordion="collapse">
 
-
               <h2 id="accordion-collapse-heading-1">
                 <button
                   type="button"
-                  className="flex items-center justify-between w-full p-5 font-medium bg-primary-150  border border-b-0 border-primary-200 rounded-t-xl hover:bg-primary-300 hover:text-white gap-3"
+                  className="flex items-center justify-between w-full p-5 font-medium bg-primary-150 border border-b-0 border-primary-200 rounded-t-xl hover:bg-primary-300 hover:text-white gap-3"
                   onClick={() => handleToggle(1)}
                   aria-expanded={openIndex === 1}
                   aria-controls="accordion-collapse-body-1"
                 >
-                  <span>What is Flowbite?</span>
+                  <span>Contactanos!</span>
                   <svg className={`w-3 h-3 ${openIndex === 1 ? 'rotate-180' : ''} shrink-0`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
                   </svg>
@@ -48,21 +47,25 @@ const Footer = () => {
                 aria-labelledby="accordion-collapse-heading-1"
               >
                 <div className="p-5 border border-b-0 border-t-0 border-primary-300 bg-primary-250">
-                  <p className="mb-2 ">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
-                  <p className="">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+                  <p className="mb-2">Si tienes alguna pregunta, comentario o necesitas más información sobre nuestras rifas, no dudes en ponerte en contacto con nosotros. Estamos aquí para ayudarte.</p>
+                  <div className="flex gap-6">
+                    <p className='text-blue-200 cursor-pointer'>info@rifapp.com</p>
+                    <p className='text-blue-400 cursor-pointer'>+57 312 123 4567</p>
+
+
+                  </div>
                 </div>
               </div>
-
 
               <h2 id="accordion-collapse-heading-2">
                 <button
                   type="button"
-                  className="flex items-center justify-between w-full bg-primary-350 p-5 font-medium text-white-500 border border-b-0 border-primary-200  hover:bg-primary-300 hover:text-white gap-3"
+                  className="flex items-center justify-between w-full bg-primary-350 p-5 font-medium text-white-500 border border-b-0 border-primary-200 hover:bg-primary-300 hover:text-white gap-3"
                   onClick={() => handleToggle(2)}
                   aria-expanded={openIndex === 2}
                   aria-controls="accordion-collapse-body-2"
                 >
-                  <span>Is there a Figma file available?</span>
+                  <span>¿Cómo funciona RIFAPP?</span>
                   <svg className={`w-3 h-3 ${openIndex === 2 ? 'rotate-180' : ''} shrink-0`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
                   </svg>
@@ -75,20 +78,19 @@ const Footer = () => {
                 aria-labelledby="accordion-collapse-heading-2"
               >
                 <div className="p-5 border border-b-0 border-t-0 border-primary-300 bg-primary-250">
-                  <p className="mb-2 ">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
-                  <p className="">Check out the <a href="https://flowbite.com/figma/" className="text-blue-600 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+                  <p className="mb-2">RIFAPP permite a los usuarios comprar boletos para rifas de forma sencilla. Una vez que se cierra la rifa, se realiza un sorteo y se anuncian los ganadores.</p>
                 </div>
               </div>
 
               <h2 id="accordion-collapse-heading-3">
                 <button
                   type="button"
-                  className="flex items-center justify-between w-full p-5 bg-primary-150 font-medium  border border-b-0 border-primary-200 rounded-b-xl hover:bg-primary-300 hover:text-white gap-3"
+                  className="flex items-center justify-between w-full p-5 bg-primary-150 font-medium border border-b-0 border-primary-200 rounded-b-xl hover:bg-primary-300 hover:text-white gap-3"
                   onClick={() => handleToggle(3)}
                   aria-expanded={openIndex === 3}
                   aria-controls="accordion-collapse-body-3"
                 >
-                  <span>What are the differences between Flowbite and Tailwind UI?</span>
+                  <span>¿Qué premios puedo ganar?</span>
                   <svg className={`w-3 h-3 ${openIndex === 3 ? 'rotate-180' : ''} shrink-0`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
                   </svg>
@@ -96,16 +98,13 @@ const Footer = () => {
               </h2>
               <div
                 ref={el => contentRefs.current[3] = el}
-
-
                 id="accordion-collapse-body-3"
                 className={`overflow-hidden transition-all duration-1000 ease-in-out ${openIndex === 3 ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`}
                 aria-labelledby="accordion-collapse-heading-3"
               >
                 <div className="p-5 border border-t-0 border-primary-300 bg-primary-250 text-white-500 rounded-b-xl">
-                  <p className="mb-2">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
-                  <p className="mb-2 ">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
-                  <p className="mb-2 ">Learn more about these technologies:</p>
+                  <p className="mb-2">En RIFAPP, ofrecemos una variedad de premios que pueden incluir desde productos electrónicos hasta experiencias únicas. Cada rifa tiene sus propios premios, así que asegúrate de revisar los detalles.</p>
+                  <p className="mb-2">¡No te pierdas la oportunidad de ganar algo increíble!</p>
                 </div>
               </div>
             </div>
--- FrontEnd/rifapp/src/views/AboutUs.jsx
+++ FrontEnd/rifapp/src/views/AboutUs.jsx
@@ -10,24 +10,131 @@ const aboutUs = () => {
     
     <section className=''>
 
+        {/* Estamos cambiando la manera en la que las personas ven las rifas */}       
         <div>
-            <h1 className='title lg:text-7xl md:text-6xl sm:text-5xl xs:text-5xl xxs:text-3xl text-center text-primary-450 font-bold mt-9'>
-                <span>Estamos cambiando la </span> <br />
-                <span>manera en la que las </span>  <br />
-                <span>personas ven las rifas</span>
+            <h1 className='title  lg:w-8/12 md:w-12/12 sm:w-10/12 xs:w-10/12 xxs:w-10/12 md m-auto    lg:text-7xl md:text-6xl sm:text-5xl xs:text-5xl xxs:text-3xl text-center text-primary-450 font-bold mt-9'>
+                Estamos cambiando la
+                manera en la que las 
+                personas ven las rifas
 
             </h1>
 
             <div className="py-0.5 my-4 bg-primary-200 border-b w-6/12 m-auto text-white"></div>
 
-            <p className='text text-center text-primary-350 mt-4 text-xl font-bold mb-4'>
-                <span>Estamos revolucionando el mercado de las rifas al ofrecer transparencia total</span> <br />
-                <span>en cada sorteo mediante tecnología verificable, máxima seguridad para nuestros</span> <br />
-                <span>usuarios con estrictos protocolos, accesibilidad desde cualquier lugar a través de</span> <br />
-                <span>nuestra plataforma online, una amplia variedad de premios para todos los gustos </span> <br />
-                <span>y una experiencia de usuario intuitiva y fácil de usar.</span> 
+            <p className='text lg:w-8/12 sm:w-10/12 xs:w-10/12 xxs:w-10/12 m-auto text-center text-primary-350 mt-4 lg:text-xl md:text-xl sm:text-lg xs:text-lg xxs:text-lg font-bold mb-4'>
+                Estamos revolucionando el mercado de las rifas al ofrecer transparencia total
+                en cada sorteo mediante tecnología verificable, máxima seguridad para nuestros
+                usuarios con estrictos protocolos, accesibilidad desde cualquier lugar a través de
+                nuestra plataforma online, una amplia variedad de premios para todos los gustos 
+                y una experiencia de usuario intuitiva y fácil de usar.
             </p>
         </div>
+
+{/* Nuestra Misión */}
+<motion.div
+        className='bg-primary-250 grid grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 xxs:grid-cols-1 items-center justify-center px-12 m-auto'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h1 className='title text-center text-6xl w-full sm:mt-4 xs:mt-4 xxs:mt-4 hover:transform hover:translate-y-3 transition-transform duration-200 cursor-pointer'>Nuestra Misión</h1>
+        <p className='text lg:text-start md:text-start text-xl py-6 w-full sm:text-center xs:text-center xxs:text-center'>
+          <span className='title '>Conectar sueños con oportunidades </span>
+          Nuestra misión es democratizar el acceso a la posibilidad de ganar, ofreciendo una plataforma segura, transparente y emocionante que conecte a las personas con sus sueños. 
+          Queremos ser el puente entre la ilusión y la realidad, brindando oportunidades únicas a través de rifas innovadoras y atractivas.
+        </p>
+      </motion.div>
+
+      {/* Sobre Nosotros */}
+      <motion.div
+        className='bg-primary-350 grid grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 xxs:grid-cols-1 items-center justify-center px-12 m-auto'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        <p className='lg:order-1 md:order-1 sm:order-2 xs:order-2 xxs:order-2 text lg:text-end md:text-start text-xl py-6 w-full sm:text-center xs:text-center xxs:text-center'>
+          <span className='title '>Redefiniendo la emoción de ganar </span>
+          En Rifapp, no solo vendemos boletos de rifa; creamos experiencias emocionantes y conectamos a personas con la posibilidad de ganar premios increíbles. Somos una plataforma innovadora que está revolucionando la forma en que se realizan las rifas, ofreciendo transparencia, seguridad y una experiencia de usuario inigualable.
+        </p>
+        <h1 className='lg:order-2 md:order-2 sm:order-1 xs:order-1 xxs:order-1 title text-center text-6xl w-full sm:mt-4 xs:mt-4 xxs:mt-4 hover:transform hover:translate-y-3 transition-transform duration-200 cursor-pointer'>Sobre Nosotros</h1>
+      </motion.div>
+
+      {/* Nuestra Fundación */}
+      <motion.div
+        className='bg-primary-250 grid grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 xxs:grid-cols-1 items-center justify-center px-12 m-auto'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2.0, delay: 0.6 }}
+      >
+        <h1 className='title text-center text-6xl w-full sm:mt-4 xs:mt-4 xxs:mt-4  hover:transform hover:translate-y-3 transition-transform duration-200 cursor-pointer'>Nuestra Fundación</h1>
+        <p className='text lg:text-start md:text-start text-xl py-6 w-full sm:text-center xs:text-center xxs:text-center'>
+          <span className='title '>Una idea nacida de la pasión por la innovación </span>
+          Rifapp nació de la visión de un grupo de emprendedores apasionados por la tecnología y la emoción de las rifas. Observamos la necesidad de modernizar un mercado tradicional, brindando una alternativa confiable y accesible para todos. Fundada en 2024, Rifapp se ha convertido rápidamente en un referente en el mundo de las rifas online.
+        </p>
+      </motion.div>
+
+
+        {/* Cards*/}  
+        <div className='my-6 grid lg:grid-cols-4 md:grid-cols-2 sm-grid-cols-2 xs:grid-cols-2 xxs:grid-cols-1 justify-center items-center gap-4 m-auto w-10/12'>
+            
+        {/* 1 */}
+        <article
+            class="w-full mx-w-full h-10/12 bg-primary-400 shadow p-4 space-y-2 rounded-xl hover:-translate-y-2 duration-300"
+            >
+        <svg viewBox="0 0 24 24" className='w-12' fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3z" fill="currentColor"/>
+        </svg>
+
+            <h1 className='title text-2xl'>María S.</h1>
+            <p class="text-md w-full text-gray-400 text">
+            "Rifapp es fácil de usar y muy confiable. ¡La variedad de premios es excelente! ¡La recomiendo mucho!"
+            </p>
+        </article>
+
+        {/* 2 */}
+        <article
+            class="w-full mx-w-full h-10/12 bg-primary-400 shadow p-4 space-y-2 rounded-xl hover:-translate-y-2 duration-300"
+            >
+        <svg viewBox="0 0 24 24" className='w-12' fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3z" fill="currentColor"/>
+        </svg>
+
+            <h1 className='title text-2xl'>Juan P.</h1>
+            <p class="text-md w-full text-gray-400 text">
+            "Con Rifapp participo en rifas accesibles. ¡Los premios son increíbles y la plataforma intuitiva!"
+            </p>
+        </article>
+
+        {/* 3 */}
+        <article
+            class="w-full mx-w-full h-10/12 bg-primary-400 shadow p-4 space-y-2 rounded-xl hover:-translate-y-2 duration-300"
+            >
+        <svg viewBox="0 0 24 24" className='w-12' fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3z" fill="currentColor"/>
+        </svg>
+
+            <h1 className='title text-2xl'>Ana G.</h1>
+            <p class="text-md w-full text-gray-400 text">
+            "Rifapp ofrece seguridad y gran facilidad para participar. ¡Una innovadora forma de ganar y promocionar!"
+            </p>
+        </article>        
+
+        {/* 4 */}
+        <article
+            class="w-full mx-w-full h-10/12 bg-primary-400 shadow p-4 space-y-2 rounded-xl hover:-translate-y-2 duration-300"
+            >
+        <svg viewBox="0 0 24 24" className='w-12' fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3z" fill="currentColor"/>
+        </svg>
+
+            <h1 className='title text-2xl'>Carlos R.</h1>
+            <p class="text-md w-full text-gray-400 text">
+            "Rifapp es sencilla de usar y el soporte es muy bueno. ¡Ya gané un premio! ¡Estoy demasiado contento!"
+            </p>
+        </article>             
+        
+        
+        </div>        
 
     </section>
 
